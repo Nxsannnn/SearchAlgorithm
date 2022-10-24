4ae36efd312404bb3541069bec85da058ef8b863
Ichsan Al Munawwar Patintingan <ichsan.al.ft21@mail.umy.ac.id>
2022-10-24 16:12:46 +0700

    Add project files.

---
+++ SearchAlgorithm/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithm
+{
+     class Program
+    {
+        int[] arr = new int[20];
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
